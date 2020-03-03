9ad5e96d87a1c206a02d9c26a6861d41a28a0103
rixong <rixong@gmail.com>
2020-03-03 13:17:34 -0600

    ready to merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,8 +29,10 @@ class UsersController < ApplicationController
  
   private
   def user_params
-    params.require(:user).permit(:name, :email, :bio, :genre_ids, :instrument_ids, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :bio, {:genre_ids => []}, {:instrument_ids => []}, :password, :password_confirmation)
   end
+
+
 
   
 end
