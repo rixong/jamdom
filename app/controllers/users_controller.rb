c391c4d503611451d2d4f3ace69137e1bb8d0c7d
rixong <rixong@gmail.com>
2020-03-03 18:48:16 -0600

    user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
+    # @user ||= User.find(params[:id])
   end
   
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+
+  root 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
